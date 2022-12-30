2b80dfd56b98924893a5b505c9d2797454bfde01
dev <aakarshsharma318@gmail.com>
2022-12-30 21:28:53 +0530

    All work

---
+++ Header.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+export default function Header(){
+    return (
+   <div>
+    <nav className="nav-items">
+        <img src="favicon.ico"/><span className="span">ReactFacts</span>
+        <ul className="list-items">
+            <li>React Course-Project 1</li>
+        </ul>
+    </nav>
+    </div>
+    )
+}
